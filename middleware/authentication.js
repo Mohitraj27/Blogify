f5884fccf28691e9ca526c57915156d0ff3e9d3d
Mohitraj27 <raj716980@gmail.com>
2024-06-29 01:00:26 -0700

    Inital Commit

---
+++ middleware/authentication.js
@@ -0,0 +1,21 @@
+const { validateToken } = require("../services/authentication");
+
+function checkForAuthenticationCookie(){
+    return (req,res,next)=>{
+        const tokenCookieValue = req.cookie[cookieName];
+        if(!tokenCookieValue){
+            next();
+        }
+       try{
+            const userPayload = validateToken(tokenCookieValue);
+            req.user = userPayload;
+        }
+        catch (error){ }
+
+        next();
+    };
+}
+
+module.exports={
+    checkForAuthenticationCookie,
+}
